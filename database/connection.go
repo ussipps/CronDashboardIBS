eae7e096b4b1d680ddaf91ec6fb42fa55ee685ec
FadhilAkbar <fadhilakbartiga@gmail.com>
2021-02-17 00:01:17 +0700

    Added Source

--- database/connection.go
+++ database/connection.go
@@ -23,20 +23,3 @@ func ConnectDB() *sql.DB {
 	db.SetConnMaxLifetime(time.Nanosecond)
 	return db
 }
-
-
-
-func ConnectDBWA() *sql.DB {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	db, err := sql.Open(os.Getenv("DB_WA_CONNECTION"), ""+os.Getenv("DB_WA_USERNAME")+":"+os.Getenv("DB_WA_PASSWORD")+"@tcp("+os.Getenv("DB_WA_HOST")+":"+os.Getenv("DB_WA_PORT")+")/"+os.Getenv("DB_WA_DATABASE")+"")
-	if err != nil {
-		panic(err)
-	}
-	db.SetMaxOpenConns(20)
-	db.SetMaxIdleConns(0)
-	db.SetConnMaxLifetime(time.Nanosecond)
-	return db
-}
--- functions/utility.go
+++ functions/utility.go
@@ -1,52 +1,9 @@
 package functions
 
 import (
-	"database/sql"
-	"fmt"
 	"github.com/op/go-logging"
-	"github.com/vjeantet/jodaTime"
-	"net"
 	"os"
-	"time"
 )
-
-func GetIpAdd() string {
-	addrs, err := net.InterfaceAddrs()
-	if err != nil {
-		_, _ = os.Stderr.WriteString("Oops: " + err.Error() + "\n")
-		os.Exit(1)
-	}
-	var ipadd = ""
-	for _, a := range addrs {
-		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To4() != nil {
-				ipadd = ipnet.IP.String()
-			}
-		}
-	}
-	return ipadd
-}
-
-//noinspection SqlDialectInspection,SqlNoDataSourceInspection
-func InsertLogCron(scheduler string, message string, conn *sql.DB) {
-	stmt, err := conn.Prepare("INSERT INTO logcron(scheduler,ip_address, message,tgl_proses) VALUES(?,?,?,?)")
-	if err != nil {
-		panic(err.Error())
-	}
-	defer stmt.Close()
-	ipAdd := GetIpAdd()
-	tglProses := jodaTime.Format("YYYY-MM-dd HH:mm:ss", time.Now())
-	stmt.Exec(scheduler, ipAdd, message, tglProses)
-}
-
-func ParseTimeScheduler(Time string) string {
-	t, err := time.Parse("15:04", Time)
-	if err != nil {
-		fmt.Println(err)
-		return ""
-	}
-	return t.Format("15:04")
-}
 
 func Logger() *logging.Logger {
 	var log = logging.MustGetLogger("main")
